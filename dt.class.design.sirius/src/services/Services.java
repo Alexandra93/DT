9e6e4fab025978a16297f78c8b096583b1c07f31
Ale <Ale@DESKTOP-MK66U22>
2017-07-05 07:39:06 +0300

    Clean up sirius

--- dt.class.design.sirius/src/services/Services.java
+++ dt.class.design.sirius/src/services/Services.java
@@ -16,15 +16,7 @@ import design.Interface;
 
 
 public class Services {
-    
-    /**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
-    }
-    
+ 
     public String computeDefaultName(final EObject element) {
  		Predicate<EObject> predicate = null;
  		String name = element.getClass().getSimpleName();
